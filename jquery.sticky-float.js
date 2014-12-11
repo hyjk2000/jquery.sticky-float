3ecc7ffc37d2cec331c27b6340c0fed53bdc3c2a
James Shih <gtx1024@gmail.com>
2014-12-11 18:02:42 +0800

    revised for new feature

--- jquery.sticky-float.js
+++ jquery.sticky-float.js
@@ -14,14 +14,25 @@
 			var x = _me.offset().left - parseFloat(_me.css('margin-left').replace(/auto/, 0));
 			var _top = _me.offset().top - parseFloat(_me.css('margin-top').replace(/auto/, 0));
 
+			// Get the y offset of container's bottom
+			// For collision detecting
+			var _container = _me.offsetParent();
+			var _y_max = _container.offset().top + _container.height() - _me.outerHeight(true);
+
 			$(window).scroll(function () {
 				// What the y position of the scroll is?
 				var y = $(this).scrollTop();
 				
 				// Whether that's below the obj.
 				if (y >= _top) {
-					// If so, fix it to the top of screen.
-					_me.attr('style', 'position:fixed;top:0;left:' + x + 'px');
+					// Whether that's below the bottom of container
+					if (y >= _y_max) {
+						// Fix it to the bottom of container
+						_me.attr('style', 'position:absolute;bottom:0');
+					} else {
+						// Fix it to the top of screen.
+						_me.attr('style', 'position:fixed;top:0;left:' + x + 'px');
+					}
 				} else {
 					// Otherwise restore it.
 					_me.removeAttr('style');
